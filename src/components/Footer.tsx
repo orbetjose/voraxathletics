76eaae0c11e1095b0dab4ced2016f1dae2d5af59
Orbet Lozada <orbetjose@gmail.com>
2026-05-18 22:44:20 -0500

    se agregaron enlaces a redes sociales

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,9 +6,9 @@ export default function Footer() {
         <div className="pt-16 pb-12">
             <img src={`${domain}wp-content/uploads/2026/05/logo_footer.png`} alt="Logo de Voraxathletics footer" className="h-7 mx-auto" />
             <div className="flex justify-center items-center gap-4 pt-16">
-                <a href="#"><img className="h-6" src={`${domain}wp-content/uploads/2026/05/fb.png`} alt="Icono de Facebook"/></a>
-                <a href="#"><img className="h-6" src={`${domain}wp-content/uploads/2026/05/instagram.png`} alt="Icono de Instagram"/></a>
-                <a href="#"><img className="h-7" src={`${domain}wp-content/uploads/2026/05/tiktok.png`} alt="Icono de TikTok"/></a>
+                <a href="https://www.facebook.com/share/1D4ahStgts/"><img className="h-6" src={`${domain}wp-content/uploads/2026/05/fb.png`} alt="Icono de Facebook"/></a>
+                <a href="https://www.instagram.com/vorax.athletics?igsh=bzUyYTk0djJmNzB2"><img className="h-6" src={`${domain}wp-content/uploads/2026/05/instagram.png`} alt="Icono de Instagram"/></a>
+                <a href="https://www.tiktok.com/@vorax.athleisure?_r=1&_t=ZS-96TtCXuDgC9"><img className="h-7" src={`${domain}wp-content/uploads/2026/05/tiktok.png`} alt="Icono de TikTok"/></a>
             </div>
             <p className="text-center text-third-black font-inter-regular pt-12">Etiam sit amet orci eget eros </p>
         </div>
